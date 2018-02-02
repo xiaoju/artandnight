b043712f405046272f8825faeae631456bd3a5d4
xiaoju <me@xiaoju.io>
2018-02-02 01:27:05 +0100

    feat: styling, using mostly components

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Menu, Icon, Container, Image } from 'semantic-ui-react';
+import { Menu, Icon, Container, Image, Grid } from 'semantic-ui-react';
 // import logo from '../assets/art_and_night-logowithText-02b.svg';
 // import logo from '../assets/kkk.png';
 
@@ -12,87 +12,91 @@ export default class NavBar extends Component {
     const { activeItem } = this.state;
 
     return (
-      <div className="ui grid">
-        <div className="column three wide">
-          <Image
-            href="/"
-            src="/assets/logo.svg"
-            fluid
-            style={{
-              'padding-right': '10px',
-              margin: '10%',
-              'min-width': '15em'
-            }}
-          />
-        </div>
+      <Grid relaxed>
+        <Grid.Row columns={2} padded>
+          <Grid.Column floated="left" width={3}>
+            <Image
+              href="/"
+              src="/assets/logo.svg"
+              fluid
+              style={{
+                //   // 'padding-right': '10px',
+                margin: '10%',
+                'min-width': '15em'
+              }}
+            />
+          </Grid.Column>
+          <Grid.Column floated="right">
+            {/* <div className="column thirteen wide right aligned"> */}
+            {/* <div className="column ten wide right floated"> */}
+            {/* <div className="column ten wide right aligned"> */}
+            {/* <div className="column ten wide right aligned"> */}
+            <Menu size="small" secondary stackable>
+              <Menu.Menu position="right">
+                <Menu.Item
+                  href="/"
+                  name="home"
+                  active={activeItem === 'home'}
+                  onClick={this.handleItemClick}
+                  color="red"
+                >
+                  Home
+                </Menu.Item>
 
-        {/* <div className="column thirteen wide right aligned"> */}
-        <div className="column ten wide right floated">
-          <Menu size="small" secondary stackable>
-            <Menu.Menu position="right">
-              <Menu.Item
-                href="/"
-                name="home"
-                active={activeItem === 'home'}
-                onClick={this.handleItemClick}
-                color="red"
-              >
-                Home
-              </Menu.Item>
+                <Menu.Item
+                  name="artists"
+                  href="/artists"
+                  active={activeItem === 'artists'}
+                  onClick={this.handleItemClick}
+                  color="red"
+                >
+                  Artists
+                </Menu.Item>
 
-              <Menu.Item
-                name="artists"
-                href="/artists"
-                active={activeItem === 'artists'}
-                onClick={this.handleItemClick}
-                color="red"
-              >
-                Artists
-              </Menu.Item>
+                <Menu.Item
+                  name="curators"
+                  href="/curators"
+                  active={activeItem === 'curators'}
+                  onClick={this.handleItemClick}
+                  color="red"
+                >
+                  Curators
+                </Menu.Item>
 
-              <Menu.Item
-                name="curators"
-                href="/curators"
-                active={activeItem === 'curators'}
-                onClick={this.handleItemClick}
-                color="red"
-              >
-                Curators
-              </Menu.Item>
+                <Menu.Item
+                  name="locations"
+                  href="/locations"
+                  active={activeItem === 'locations'}
+                  onClick={this.handleItemClick}
+                  color="red"
+                >
+                  Locations
+                </Menu.Item>
 
-              <Menu.Item
-                name="locations"
-                href="/locations"
-                active={activeItem === 'locations'}
-                onClick={this.handleItemClick}
-                color="red"
-              >
-                Locations
-              </Menu.Item>
+                <Menu.Item
+                  name="agenda"
+                  href="/agenda"
+                  active={activeItem === 'agenda'}
+                  onClick={this.handleItemClick}
+                  color="red"
+                >
+                  Agenda
+                </Menu.Item>
 
-              <Menu.Item
-                name="agenda"
-                href="/agenda"
-                active={activeItem === 'agenda'}
-                onClick={this.handleItemClick}
-                color="red"
-              >
-                Agenda
-              </Menu.Item>
-
-              <Menu.Item
-                name="about"
-                href="/about"
-                active={activeItem === 'about'}
-                onClick={this.handleItemClick}
-                color="red"
-              >
-                About
-              </Menu.Item>
-            </Menu.Menu>
-          </Menu>
-        </div>
-      </div>
+                <Menu.Item
+                  name="about"
+                  href="/about"
+                  active={activeItem === 'about'}
+                  onClick={this.handleItemClick}
+                  color="red"
+                >
+                  About
+                </Menu.Item>
+              </Menu.Menu>
+            </Menu>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     );
   }
 }
