6359f393624ed0afe2273d9a030c8c4926783d42
xiaoju <me@xiaoju.io>
2018-02-06 22:46:15 +0100

    feat: footer with 2 elements of text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import { Segment } from 'semantic-ui-react';
+import { Grid } from 'semantic-ui-react';
 
 function Footer() {
   return (
-    <Segment padded="very" textAlign="center" size="small">
-      ©2018 by art and night
-    </Segment>
+    <Grid
+      textAlign="center"
+      stackable
+      style={{ padding: '1rem 1rem !important' }}
+    >
+      <Grid.Row divided>
+        <Grid.Column width={3}>© ART AND NIGHT</Grid.Column>
+        <Grid.Column width={3}>
+          Réalisation <a href="http://www.xiaoju.io">xiaoju.io</a>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
   );
 }
+
+// also changed
+// padding: 0rem 0rem !important
+// into
+// .ui.grid > .stackable.stackable.row > .column {
+// of
+// semantic-ui-css/semantic.css
 
 export default Footer;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import 'semantic-ui-css/semantic.min.css';
+// import 'semantic-ui-css/semantic.min.css';
+import 'semantic-ui-css/semantic.css';
 import './index.css';
 
 ReactDOM.render(<App />, document.querySelector('#root'));
