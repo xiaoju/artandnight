457625eae024d3c25655fc6a15517fd687b80eef
xiaoju <me@xiaoju.io>
2018-02-06 22:44:54 +0100

    feat: add text overlay on top of background image for landing page

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,9 +2,21 @@ import React from 'react';
 import { Image } from 'semantic-ui-react';
 
 const Home = () => (
-  <div>
-    <Image src="/assets/home.jpg" fluid />
-  </div>
+  <Image fluid>
+    <Image src="/assets/home.jpg" />
+    <div
+      style={{
+        color: 'white',
+        'font-size': '4rem',
+        position: 'absolute',
+        top: '33%',
+        left: '50%',
+        transform: 'translateX(-50%) translateY(-50%)'
+      }}
+    >
+      ART AND NIGHT
+    </div>
+  </Image>
 );
 
 export default Home;
